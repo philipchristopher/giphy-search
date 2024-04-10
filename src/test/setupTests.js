2afc8e4483e7e344b9307f8dbc2baadb0974ded9
philipchristopher <philip_christopher@outlook.com>
2024-04-10 15:42:58 +1000

    Add setupTests.js file for configuring the testing framework

---
+++ src/test/setupTests.js
@@ -0,0 +1,7 @@
+/**
+ * This file is automatically run before each test file in the project.
+ *
+ * Adding common packages here instead of manually importing them each
+ * time.
+ */
+require("@testing-library/jest-dom");
