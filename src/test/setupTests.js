86d6cf1a798a8c02af43f5deb2ad822a83a08a73
philipchristopher <philip_christopher@outlook.com>
2024-04-11 18:33:26 +1000

    Update Jest configuration and test setup files

--- src/test/setupTests.js
+++
@@ -1,7 +0,0 @@
-/**
- * This file is automatically run before each test file in the project.
- *
- * Adding common packages here instead of manually importing them each
- * time.
- */
-require("@testing-library/jest-dom");
