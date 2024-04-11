b78121ced2ba28968a27447596fa552618f48205
philipchristopher <philip_christopher@outlook.com>
2024-04-11 20:29:46 +1000

    feat: add UI to save image

--- src/components/GifCard.tsx
+++ src/components/GifCard.tsx
@@ -1,26 +1,47 @@
+import { useGifContext } from "../context/useGifContext";
 import { Gif } from "../data-access/types";
+import { FaHeart } from "../icons/FaHeart";
 
-type GifCardProps = Pick<Gif, "images" | "title" | "url">;
+type GifCardProps = Pick<Gif, "id" | "images" | "title">;
 
-const GifCard: React.FC<GifCardProps> = ({ images, title, url }) => {
+/**
+ * Displays a single gif card. The card has an image and a title. The image can
+ * be saved by clicking on the card.
+ */
+const GifCard: React.FC<GifCardProps> = ({ id, images, title }) => {
+  const { addImageId, removeImageId, imageIds, isImageSaved } = useGifContext();
+  const isActive = isImageSaved(id);
+  const handleSaveImage = () => {
+    if (imageIds.includes(id)) {
+      removeImageId(id);
+      return;
+    }
+    addImageId(id);
+  };
+
   return (
-    <a
-      className="flex flex-col justify-start group overflow-hidden w-[200px]"
-      href={url}
-      rel="noreferrer noopener"
-      target="_blank"
+    <button
+      className={`flex flex-col justify-start group overflow-hidden w-[200px] group rounded-xl relative ${
+        isActive ? "outline outline-rose-500 outline-offset-2 outline-4" : ""
+      }`}
+      onClick={handleSaveImage}
     >
       <img
         width={200}
         height={200}
         src={images.fixed_width_downsampled.url}
         alt={title}
-        className="rounded-xl mb-2 bg-slate-200 object-cover h-[200px]"
+        className="rounded-xl mb-2 bg-slate-200 object-cover h-[200px] group-hover:scale-110 group-hover:grayscale transition-[transform,colors,filter] group-hover:brightness-[0.3]"
+      />
+      <FaHeart
+        className={`top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 absolute fill-white scale-[5] transition-transform group-hover:opacity-100 opacity-0 ${
+          isActive ? "scale-[8] fill-rose-400" : ""
+        }`}
       />
       <p className="text-transparent transition-colors -mt-9 rounded-b-lg py-1 px-4 font-semibold drop-shadow-md group-hover:bg-slate-800/30 group-hover:text-white text-sm truncate capitalize">
         {title}
       </p>
-    </a>
+    </button>
   );
 };
 
--- src/components/GifList.tsx
+++ src/components/GifList.tsx
@@ -8,13 +8,8 @@ type GifListProps = {
 const GifList: React.FC<GifListProps> = ({ gifs }) => {
   return (
     <div className="flex flex-wrap gap-4 justify-center">
-      {gifs.map(({ id, url, images, title }, index) => (
-        <GifCard
-          key={`${index}-${id}`}
-          images={images}
-          title={title}
-          url={url}
-        />
+      {gifs.map(({ id, images, title }, index) => (
+        <GifCard key={`${index}-${id}`} id={id} images={images} title={title} />
       ))}
     </div>
   );
